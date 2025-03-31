bf331f7610df83b78cc2feef38a25f90d5e5c221
varad-n <hvi924926@gmail.com>
2025-03-31 23:09:05 +0530

    project in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import Dishes from './components/Dishes'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className='overflow-y-hidden text-neutral-200 antialiased'>
     <HeroSection />
     <Navbar />
+    <Dishes />
+    <About />
   </main>
   )
 }
